a2d158e1f18adc2016ed7614393bc1652a378652
magnutog12 <121357580+magnutog12@users.noreply.github.com>
2023-08-15 15:04:44 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import openai
+import gradio
+
+openai.api_key = "YOUR API KEY HERE"
+
+messages = [{"role": "system", "content": "YOUR CUSTOM TEXT HERE TO TRAIN THE LLM"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Chat With Harry")
+
+demo.launch(share=True)
+
